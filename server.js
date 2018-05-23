a3758cbcca928b9078ba3a9d91db8e168f6b30cd
Yuriy Makov <juramak98@gmail.com>
2018-05-23 19:32:49 +0300

    added users list

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5433,
+    user: 'yuriy',
+    database: 'kredytDB',
+    password: '1111'
+  },
+})
+
+
+app.get('/users', async function (req, res) {
+  res.send(await knex.select().from('klient_profile'))
+})
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('Example app listening on port 8080!')
+})
